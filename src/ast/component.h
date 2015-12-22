5058d1f2c8d1ff70b5235dedc25859542885df9b
Sam Kellett <samkellett@gmail.com>
2015-12-22 17:57:57 +0000

    Added AST representation of a component.

---
+++ src/ast/component.h
@@ -0,0 +1,30 @@
+#ifndef AST_COMPONENT_H
+#define AST_COMPONENT_H
+
+#include "component/list.h"
+
+#include "utils/meta/apply.h"
+#include "utils/meta/transform.h"
+
+#include <eggs/variant.hpp>
+
+namespace gxy::ast {
+
+// A representation of a component in an initial state. Useful for building up a scene tree
+// in an AST as stated by a configuration.
+template <typename T>
+class component_facade
+{
+public:
+  typename T::data data;
+  typename T::const_data const_data;
+};
+
+// An ast::component can be any of our defined components.
+using component = meta::apply_t<eggs::variant,
+  meta::transform_t<components::list, component_facade>>;
+
+} // namespace gxy::ast
+
+#endif // AST_COMPONENT_H
+
---
+++ test/ast/test_component.cpp
@@ -0,0 +1,28 @@
+#include "ast/component.h"
+
+using namespace gxy;
+
+struct stub_component
+{
+  using data = int;
+  using const_data = bool;
+};
+
+using uut = ast::component_facade<stub_component>;
+
+// Check that the types of our data has successfully propagated into the facade.
+static_assert(std::is_same<decltype(uut().data), int>{});
+static_assert(std::is_same<decltype(uut().const_data), bool>{});
+
+// All existing components should be accessible in our ast::component variant.
+using expected = eggs::variant<
+  ast::component_facade<components::transform>
+>;
+
+static_assert(std::is_same<expected, ast::component>{});
+
+int main()
+{
+  return 0;
+}
+
