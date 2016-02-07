e24bc61d6757f98ff40acf0ed29926505d1eb6d3
Sam Kellett <samkellett@gmail.com>
2016-02-07 14:41:33 +0000

    Added unit test checking for OpenGL version.

---
+++ test/test_opengl.cpp
@@ -0,0 +1,35 @@
+#include "gl/context.h"
+#include "gl/environment.h"
+
+#include <GLFW/glfw3.h>
+
+#include <gtest/gtest.h>
+
+using namespace gxy;
+
+struct Fixture : public testing::Test
+{
+  int width{42};
+  int height{89};
+  std::string title{"badger"};
+
+  gl::environment env{};
+  gl::context ctx{env, width, height, title.c_str()};
+};
+
+TEST_F(Fixture, OpenGLMajorVersion)
+{
+  int major{};
+  ::glGetIntegerv(GL_MAJOR_VERSION, &major);
+
+  ASSERT_EQ(4, major);
+}
+
+TEST_F(Fixture, OpenGLMinorVersion)
+{
+  int minor{};
+  ::glGetIntegerv(GL_MINOR_VERSION, &minor);
+
+  ASSERT_EQ(1, minor);
+}
+
